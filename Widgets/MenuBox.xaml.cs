84745d93e997695e2b9a5cd372281703eae4cc2b
MkSerdyuk <git_user@mail.ru>
2022-07-20 20:32:48 +0300

    Сделан основной интерфейс Реализовать систему выбора траниц

---
+++ Widgets/MenuBox.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ren_Py_Designer.Widgets
+{
+    /// <summary>
+    /// Interaction logic for MenuBox.xaml
+    /// </summary>
+    public partial class MenuBox : UserControl
+    {
+        public MenuBox()
+        {
+            InitializeComponent();
+            this.Content.
+        }
+    }
+}
